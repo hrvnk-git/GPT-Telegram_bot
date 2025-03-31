ac5d8e0724bfa6d458f5d38a95ac87097d8f1c25
hrvnk <ruslanhoroshko@gmail.com>
2025-03-31 14:57:05 +0300

    fix

--- app/handlers.py
+++ app/handlers.py
@@ -1,7 +1,7 @@
 from aiogram import Bot, F, Router
 from aiogram.filters import Command
 from aiogram.types import BufferedInputFile, CallbackQuery, FSInputFile, Message
-from gpt_module import ChatGPT
+from .gpt_modulegpt_module import ChatGPT
 
 router = Router()
 
