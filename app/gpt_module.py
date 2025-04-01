04640ef2ee80bb3c3b969ef717c7d4effcc49497
hrvnk <ruslanhoroshko@gmail.com>
2025-04-02 02:49:10 +0300

    added work wit photo and now bot uses Responses(before was Completions

--- app/gpt_module.py
+++ app/gpt_module.py
@@ -1,3 +1,4 @@
+import json
 import os
 from pathlib import Path
 
@@ -7,7 +8,7 @@ from openai import AsyncOpenAI
 
 from config import instructions, prompt
 
-from .db import load_history, save_message
+from .db import load_history, load_response_id, save_message, save_response_id
 
 load_dotenv()
 
@@ -19,18 +20,36 @@ class ChatGPT:
     def __init__(self):
         self.client = AsyncOpenAI(api_key=OPENAI_API_KEY)
 
+    # async def generate_text(self, user_text, user_id: int) -> str:
+    #     user_history = await load_history(user_id, HISTORY_MAX_LEN)
+    #     user_history.append({"role": "user", "content": user_text})
+    #     messages = [{"role": "developer", "content": prompt}] + user_history
+    #     completion = await self.client.chat.completions.create(
+    #         model="o3-mini",
+    #         messages=messages,  # type: ignore
+    #     )
+    #     response_text = str(completion.choices[0].message.content)
+    #     await save_message(user_id, "user", user_text)
+    #     await save_message(user_id, "assistant", response_text)
+    #     return response_text
+
     async def generate_text(self, user_text, user_id: int) -> str:
-        user_history = await load_history(user_id, HISTORY_MAX_LEN)
-        user_history.append({"role": "user", "content": user_text})
-        messages = [{"role": "developer", "content": prompt}] + user_history
-        completion = await self.client.chat.completions.create(
-            model="o3-mini",
-            messages=messages,  # type: ignore
+        previous_response_id = await load_response_id(user_id)
+        response = await self.client.responses.create(
+            model="gpt-4o-mini",
+            input=user_text,
+            text={"format": {"type": "text"}},
+            # tools=[],
+            temperature=1,
+            max_output_tokens=2048,
+            top_p=1,
+            reasoning=None,
+            previous_response_id=previous_response_id,
+            store=True,
         )
-        response_text = str(completion.choices[0].message.content)
-        await save_message(user_id, "user", user_text)
-        await save_message(user_id, "assistant", response_text)
-        return response_text
+        await save_response_id(user_id=user_id, response_id=response.id)
+        return response.output[0].content[0].text
+        # self.response_id = response.id
 
     async def generate_voice(self, user_id: int, voice):
         transcript = await self.client.audio.transcriptions.create(
@@ -54,6 +73,32 @@ class ChatGPT:
             model="gpt-4o-mini-transcribe", file=voice
         )
         return await self.generate_text(transcript.text, user_id)
+
+    async def recieve_photo(self, user_id: int, message_text: str | None, url: str):
+        if message_text:
+            message_text = message_text
+        else:
+            message_text = "что на фото?"
+        previous_response_id = await load_response_id(user_id)
+        response = await self.client.responses.create(
+            model="gpt-4o-mini",
+            input=[
+                {
+                    "role": "user",
+                    "content": [
+                        {"type": "input_text", "text": message_text},
+                        {"type": "input_image", "image_url": url},
+                    ],
+                }  # type: ignore
+            ],
+            temperature=1,
+            max_output_tokens=2048,
+            top_p=1,
+            reasoning=None,
+            previous_response_id=previous_response_id,
+        )
+        await save_response_id(user_id=user_id, response_id=response.id)
+        return response.output[0].content[0].text
 
 
 gpt_client = ChatGPT()
